27404355f349736d52cdfb972ed0b7596e3be7f7
Fajar Septian Nugraha <fajarsn99@gmail.com>
2020-12-12 00:58:07 +0700

    Added Login Validation

--- app/src/main/java/com/pens/it/d4b2018/clara_android/mvvm/view/authentication/AuthActivity.kt
+++ app/src/main/java/com/pens/it/d4b2018/clara_android/mvvm/view/authentication/AuthActivity.kt
@@ -1,5 +1,6 @@
 package com.pens.it.d4b2018.clara_android.mvvm.view.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -7,14 +8,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.asLiveData
 import com.pens.it.d4b2018.clara_android.R
 import com.pens.it.d4b2018.clara_android.mvvm.data.local.UserPreferences
+import com.pens.it.d4b2018.clara_android.mvvm.view.main.MainActivity
+import com.pens.it.d4b2018.clara_android.mvvm.view.utils.startNewActivity
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
         val userPreferences = UserPreferences(this)
         userPreferences.authToken.asLiveData().observe(this, Observer {
-            Toast.makeText(this, it ?: "Token is Null", Toast.LENGTH_SHORT).show()
+            if (it == null) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                this.finish()
+            } else
+                setContentView(R.layout.activity_auth)
         })
     }
 }
---
+++ app/src/main/java/com/pens/it/d4b2018/clara_android/mvvm/view/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pens.it.d4b2018.clara_android.mvvm.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pens.it.d4b2018.clara_android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
